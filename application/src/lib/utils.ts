8bb645d54e3fa5ab72b24c0d25d2f5faadf0defd
Nicolás Martínez <nmartinez.dev@gmail.com>
2025-03-24 15:26:24 -0300

    Agrego función para formatear números.

--- application/src/lib/utils.ts
+++ application/src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatNumber = (num: number, isPrice: boolean = false): string => {
+  const number = num.toLocaleString('es-ES');
+
+  if (isPrice) return `${number} €`;
+
+  return number;
+};
